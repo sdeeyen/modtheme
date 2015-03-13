6c9109a33d9229a442052f0624dc2652d382d9df
mattbc <mattchan@mattbook.local>
2015-03-13 10:45:22 -0700

    Revert ef6e220..f537e5b
    This rolls back to commit ef6e2202f6e3900aca0eff5593c4b41128e7f4a8.

--- content.php
+++ content.php
@@ -6,19 +6,12 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title">
-			<?php the_title(); ?>
-			<span class="creator">
-				<?php the_field('first name'); ?>
-			</span>
-		</h1>
-		
-		<!--.entry-meta-->
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php mod_posted_on(); ?>
-		</div> 
-		
+		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
