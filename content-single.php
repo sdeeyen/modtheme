269b7080cdabc615b21f05ce4f1c8ad0b925ea0d
mattbc <mattchan@mattbook.local>
2015-03-17 18:31:10 -0700

    added second text block for artist info/template

--- content-single.php
+++ content-single.php
@@ -52,6 +52,46 @@
 					Cras bibendum dui ipsum, in ultrices elit congue id. In viverra lacus nulla, mattis hendrerit est tincidunt in. Sed tincidunt feugiat mauris vel pulvinar. Praesent velit lorem, iaculis non placerat non, sodales eget arcu. Suspendisse ullamcorper dui quis mi tristique tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante quam, volutpat bibendum dolor vel, vulputate venenatis nisi. Ut finibus arcu nunc, quis facilisis libero maximus eu. Nam maximus varius facilisis. Integer congue consequat massa eu pulvinar. Suspendisse hendrerit tellus non eleifend porttitor. Praesent rhoncus felis vitae turpis porttitor, quis vestibulum metus ultricies.
 				</p>
 			</div>
+			<!-- Artist-->
+
+			<div class="movie-poster">
+			<!-- Swap with Artist Picture -->
+				<?php the_content(); ?>
+				<?php
+				// check if post has as post thumbail assigned to it
+					if ( has_post_thumbnail() ) {
+						echo '<figure class="poster">';
+						the_post_thumbnail();
+						echo '</figure>';
+					}
+				?>
+			</div>
+			<div class="artist-statement">
+				<header class="movie-info">
+					<h1 class="entry-title">
+			                <?php the_field('first_name'); ?> <?php the_field('last_name'); ?>
+		        	</h1>
+					
+					<!-- swap these with CONTACT info-->
+			        <div class="meta-content">
+			            <span class="time"><?php 
+			            	echo get_the_term_list( $post->ID, 'durations', '', ', ' ); 
+			                ?>
+			            </span> | 
+			            <span class="genre">
+			                <?php 
+			                echo get_the_term_list( $post->ID, 'genres', '', ', ' ); 
+			                ?>
+			       		 </span> | 
+			       		 <span class="trailer-link">
+			       		 	<a href="#">Trailer</a>
+			       		 </span> 
+			        </div><!-- .meta-content -->
+				</header><!-- .entry-header -->
+				<p>
+					Cras bibendum dui ipsum, in ultrices elit congue id. In viverra lacus nulla, mattis hendrerit est tincidunt in. Sed tincidunt feugiat mauris vel pulvinar. Praesent velit lorem, iaculis non placerat non, sodales eget arcu. Suspendisse ullamcorper dui quis mi tristique tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante quam, volutpat bibendum dolor vel, vulputate venenatis nisi. Ut finibus arcu nunc, quis facilisis libero maximus eu. Nam maximus varius facilisis. Integer congue consequat massa eu pulvinar. Suspendisse hendrerit tellus non eleifend porttitor. Praesent rhoncus felis vitae turpis porttitor, quis vestibulum metus ultricies.
+				</p>
+			</div>
 
 			<?php
 				wp_link_pages( array(
@@ -60,6 +100,8 @@
 				) );
 			?>
 		</div><!-- .entry-content -->
+
+
 
 		<footer class="entry-footer">
 			<?php mod_entry_footer(); ?>
