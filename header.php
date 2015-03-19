5f3b65f429c9d6bbdf289248558f3cb7ad511df3
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-03-19 14:31:07 -0700

    Mobile-first main menu with toggle. Breakpoint currently at 800px.

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'mod' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Menu', 'mod' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
