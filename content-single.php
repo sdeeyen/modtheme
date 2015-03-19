c5b591e8f14b0dd0a62c3d34ff2b023dfab04d1e
mattbc <mattchan@mattbook.local>
2015-03-19 13:32:46 -0700

    Artist Info
    Artist Info linked from Advanced Custom Fields

--- content-single.php
+++ content-single.php
@@ -10,87 +10,88 @@
 		<div class="the-movie embed-container" >
 			<iframe src='http://player.vimeo.com/video/121144312' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 		</div>
-
-
 		<div class="entry-content">
-			<div class="movie-poster">
-				<?php the_content(); ?>
-				<?php
-				// check if post has as post thumbail assigned to it
-					if ( has_post_thumbnail() ) {
-						echo '<figure class="poster">';
-						the_post_thumbnail();
-						echo '</figure>';
-					}
-				?>
+			
+			<!-- Movie -->
+			<div class="movie-content">
+				<div class="movie-poster">
+					<?php the_content(); ?>
+					<?php
+					// check if post has as post thumbail assigned to it
+						if ( has_post_thumbnail() ) {
+							echo '<figure class="poster">';
+							the_post_thumbnail();
+							echo '</figure>';
+						}
+					?>
+				</div>
+				<!-- Movie-info -->
+				<div class="artist-statement">
+					<header class="movie-info">
+						<h1 class="entry-title">
+						  	<?php the_title(); ?> 
+				            <span class="creator">by 
+				                <?php the_field('first_name'); ?> <?php the_field('last_name'); ?>
+				            </span>
+			        	</h1>
+						
+				        <div class="meta-content">
+				            <span class="time"><?php 
+				            	echo get_the_term_list( $post->ID, 'durations', '', ', ' ); 
+				                ?>
+				            </span> | 
+				            <span class="genre">
+				                <?php 
+				                echo get_the_term_list( $post->ID, 'genres', '', ', ' ); 
+				                ?>
+				       		 </span> | 
+				       		 <span class="trailer-link">
+				       		 	<a href="#">Trailer</a>
+				       		 </span> 
+				        </div><!-- .meta-content -->
+					</header><!-- .entry-header -->
+					<div class="hr"><hr/></div>
+					<p>
+						Cras bibendum dui ipsum, in ultrices elit congue id. In viverra lacus nulla, mattis hendrerit est tincidunt in. Sed tincidunt feugiat mauris vel pulvinar. Praesent velit lorem, iaculis non placerat non, sodales eget arcu. Suspendisse ullamcorper dui quis mi tristique tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante quam, volutpat bibendum dolor vel, vulputate venenatis nisi. Ut finibus arcu nunc, quis facilisis libero maximus eu. Nam maximus varius facilisis. Integer congue consequat massa eu pulvinar. Suspendisse hendrerit tellus non eleifend porttitor. Praesent rhoncus felis vitae turpis porttitor, quis vestibulum metus ultricies.
+					</p>
+				</div>
 			</div>
-			<div class="artist-statement">
-				<header class="movie-info">
-					<h1 class="entry-title">
-					  	<?php the_title(); ?> 
-			            <span class="creator">by 
-			                <?php the_field('first_name'); ?> <?php the_field('last_name'); ?>
-			            </span>
-		        	</h1>
-					
-			        <div class="meta-content">
-			            <span class="time"><?php 
-			            	echo get_the_term_list( $post->ID, 'durations', '', ', ' ); 
-			                ?>
-			            </span> | 
-			            <span class="genre">
-			                <?php 
-			                echo get_the_term_list( $post->ID, 'genres', '', ', ' ); 
-			                ?>
-			       		 </span> | 
-			       		 <span class="trailer-link">
-			       		 	<a href="#">Trailer</a>
-			       		 </span> 
-			        </div><!-- .meta-content -->
-				</header><!-- .entry-header -->
-				<p>
-					Cras bibendum dui ipsum, in ultrices elit congue id. In viverra lacus nulla, mattis hendrerit est tincidunt in. Sed tincidunt feugiat mauris vel pulvinar. Praesent velit lorem, iaculis non placerat non, sodales eget arcu. Suspendisse ullamcorper dui quis mi tristique tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante quam, volutpat bibendum dolor vel, vulputate venenatis nisi. Ut finibus arcu nunc, quis facilisis libero maximus eu. Nam maximus varius facilisis. Integer congue consequat massa eu pulvinar. Suspendisse hendrerit tellus non eleifend porttitor. Praesent rhoncus felis vitae turpis porttitor, quis vestibulum metus ultricies.
-				</p>
-			</div>
-			<!-- Artist-->
 
-			<div class="movie-poster">
-			<!-- Swap with Artist Picture -->
-				<?php the_content(); ?>
-				<?php
-				// check if post has as post thumbail assigned to it
-					if ( has_post_thumbnail() ) {
-						echo '<figure class="poster">';
-						the_post_thumbnail();
-						echo '</figure>';
-					}
-				?>
-			</div>
-			<div class="artist-statement">
-				<header class="movie-info">
-					<h1 class="entry-title">
-			                <?php the_field('first_name'); ?> <?php the_field('last_name'); ?>
-		        	</h1>
-					
-					<!-- swap these with CONTACT info-->
-			        <div class="meta-content">
-			            <span class="time"><?php 
-			            	echo get_the_term_list( $post->ID, 'durations', '', ', ' ); 
-			                ?>
-			            </span> | 
-			            <span class="genre">
-			                <?php 
-			                echo get_the_term_list( $post->ID, 'genres', '', ', ' ); 
-			                ?>
-			       		 </span> | 
-			       		 <span class="trailer-link">
-			       		 	<a href="#">Trailer</a>
-			       		 </span> 
-			        </div><!-- .meta-content -->
-				</header><!-- .entry-header -->
-				<p>
-					Cras bibendum dui ipsum, in ultrices elit congue id. In viverra lacus nulla, mattis hendrerit est tincidunt in. Sed tincidunt feugiat mauris vel pulvinar. Praesent velit lorem, iaculis non placerat non, sodales eget arcu. Suspendisse ullamcorper dui quis mi tristique tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante quam, volutpat bibendum dolor vel, vulputate venenatis nisi. Ut finibus arcu nunc, quis facilisis libero maximus eu. Nam maximus varius facilisis. Integer congue consequat massa eu pulvinar. Suspendisse hendrerit tellus non eleifend porttitor. Praesent rhoncus felis vitae turpis porttitor, quis vestibulum metus ultricies.
-				</p>
+		<!-- Artist-->
+			<div class="artist-content">
+				<!-- Artist Picture -->
+				<div class="artist-picture">
+					<?php the_content(); ?>
+					<?php 
+						$image = get_field('profile_picture');
+						if( !empty($image) ): ?>
+							<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+
+					<?php endif; ?>
+				</div>
+				<!-- Artist info-->
+				<div class="artist-statement">
+					<header class="movie-info">
+						<h1 class="entry-title">
+				                <?php the_field('first_name'); ?> <?php the_field('last_name'); ?>
+			        	</h1>
+				        <div class="meta-content">
+				        	<span class="artist-major">
+				            	<a href="#">
+				            		<?php echo get_the_term_list( $post->ID, 'major', '', ', ' );?>
+				            	</a>
+				            </span> | 
+				            <span class="artist-email">
+				            	<a href="#"><?php the_field('artist_email'); ?></a>
+				            </span> | 
+				            <span class="artist-website">
+				                <a href="#"><?php the_field('artist_website'); ?></a>
+				       		 </span>
+				        </div><!-- .meta-content -->
+					</header><!-- .entry-header -->
+					<div class="hr"><hr/></div>
+					<p><?php the_field('artist_statement'); ?></p>
+				</div>
 			</div>
 
 			<?php
